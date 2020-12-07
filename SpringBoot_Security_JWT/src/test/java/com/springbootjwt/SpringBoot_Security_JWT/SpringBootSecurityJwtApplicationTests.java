3d16eb2b9ba267e2bc70977a4621cbb3a0c68397
Nayan <nayan.kadam1307@gmail.com>
2020-12-07 22:04:19 +0530

    Spring Security

---
+++ SpringBoot_Security_JWT/src/test/java/com/springbootjwt/SpringBoot_Security_JWT/SpringBootSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootjwt.SpringBoot_Security_JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
